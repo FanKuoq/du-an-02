0a03af999995b6c137282ca6c7b3882afd2682c3
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2023-11-12 12:21:58 +0700

    done

---
+++ index.js
@@ -0,0 +1,7 @@
+let list = document.querySelector('.feedback .feedback-list')
+let items = document.querySelectorAll('.feedback .feedback-list .feedback-item')
+let dots = document.querySelectorAll('.feedback .feedback-item .dot')
+
+let active = 0;
+let lengthItems = items.length - 1;
+
